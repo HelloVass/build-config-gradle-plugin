0483e1d9729f834f41a02ec51a3b72974cdc25d8
hellovass <chenke@hyperchain.cn>
2021-09-24 09:50:24 +0800

    jitpack.io

--- buildconfig-gradle-plugin/build.gradle.kts
+++ buildconfig-gradle-plugin/build.gradle.kts
@@ -10,12 +10,13 @@ version = "1.0.0-SNAPSHOT"
 repositories {
     google()
     mavenCentral()
+    maven { setUrl("https://jitpack.io") }
 }
 
 dependencies {
     implementation(gradleApi())
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-    implementation("com.squareup:kotlinpoet:+") {
+    implementation("com.squareup:kotlinpoet:1.10.1") {
         exclude(group = "org.jetbrains.kotlin")
     }
 }
@@ -28,4 +29,3 @@ gradlePlugin {
         }
     }
 }
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,6 @@
 rootProject.name = "build-config"
 
-include(":buildconfig-gradle-plugin",":test-lib")
-
-buildscript {
-
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        google()
-    }
-
-    dependencies {
-        classpath("info.hellovass.buildconfig_gradle_plugin:buildconfig-gradle-plugin:1.0.0-SNAPSHOT")
-    }
-}
+include(
+    ":buildconfig-gradle-plugin",
+    ":test-lib"
+)
