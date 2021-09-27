231bc9c7a0f7eb1a0a02c4df09f82bca3be4453c
chenke <chenke@xd.com>
2021-09-27 19:56:52 +0800

    完善 sample

---
+++ sample/src/main/java/info/hellovass/sample/Constant.kt
@@ -0,0 +1,12 @@
+package info.hellovass.sample
+
+import info.hellovass.buildconfig_gradle_plugin.BuildConfig
+
+object Constant {
+
+    const val NAME = BuildConfig.NAME
+
+    const val GROUP = BuildConfig.GROUP
+
+    const val VERSION = BuildConfig.VERSION
+}
